d90a368da7e42b64daaf2f3c351b23badc01e578
kenekizzy <ndukasquad@gmail.com>
2022-05-03 07:07:49 +0100

    working directory

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool
+require("dotenv").config()
+
+// const devConfig = {
+//     user: process.env.PG_USER,
+//     password: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE
+// }
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    user: "postgres",
-    password:"bube2003",
-    host: "localhost",
-    port: 5432,
-    database: "todo"
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
 })
 
 module.exports = pool
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const app = express()
 const pool = require("./db")
+const path = require("path")
 
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "client/build")))
+}
 
 //Sending data to the database
 app.post("/todos", async(req, res) =>{
@@ -68,6 +72,10 @@ app.delete("/todos/:id", async(req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log(`Server is running on port 5000`)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
